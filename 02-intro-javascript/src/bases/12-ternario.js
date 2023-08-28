73fafc7ad22a98f4aaa843c440aab478d08433d4
chnicolasdev <christian.cnod@gmail.com>
2023-08-28 15:56:30 -0400

    Capítulo 3: Clase 26 Ternario

---
+++ 02-intro-javascript/src/bases/12-ternario.js
@@ -0,0 +1,17 @@
+
+
+const activo = true;
+
+// let mensaje = '';
+
+// if ( activo ) {
+//   mensaje = 'Activo';
+// } else {
+//   mensaje = 'Inactivo';
+// }
+
+// const mensaje = ( activo ) ? 'Activo' : 'Inactivo';
+
+const mensaje = activo && 'Activo'; // Solo muestra una condición, es solo un IF
+
+console.log( mensaje );
